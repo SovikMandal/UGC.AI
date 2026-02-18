4acbb29de1902bb2072cbe1414057b1551d0d1ec
Sovik Mandal <mandalsouvik9635@gamil.com>
2026-02-18 23:42:36 +0530

    Completed Frontend Part

--- Frontend/src/pages/Loading.tsx
+++ Frontend/src/pages/Loading.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Loader2Icon } from "lucide-react"
+import { useEffect } from "react"
 
 const Loading = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = '/'
+    }, 6000)
+  }, [])
+
   return (
-    <div>
-      
+    <div className="h-screen flex flex-col">
+      <div className="flex items-center justify-center flex-1">
+        <Loader2Icon className="size-10 animate-spin text-indigo-300"/>
+      </div>
     </div>
   )
 }
--- Frontend/src/pages/MyGenerations.tsx
+++ Frontend/src/pages/MyGenerations.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import type { Project } from "../types"
+import { dummyGenerations } from "../assets/assets"
+import { Loader2Icon } from "lucide-react"
+import ProjectCard from "../components/ProjectCard"
+import { PrimaryButton } from "../components/Buttons"
 
 const MyGenerations = () => {
-  return (
-    <div>
-      
+    const [generations, setGenerations] = useState<Project[]>([])
+        const [loading, setLoading] = useState(true)
+        
+        const fetchMyGenerations = async () => {
+            setTimeout(() => {
+                setGenerations(dummyGenerations);
+                setLoading(false);
+            }, 3000)
+        }
+    
+        useEffect(() => {
+            fetchMyGenerations();
+        }, [])
+  return loading ?  (
+    <div className="flex items-center justify-center min-h-screen">
+      <Loader2Icon className="size-7 animate-spin text-indigo-400" />
+    </div>
+  ) : (
+    <div className="min-h-screen text-white p-6 md:p-12 my-28">
+        <div className="max-w-6xl mx-auto">
+            <header className="mb-12">
+                <h1 className="text-3xl md:text-4xl font-semibold mb-2">My Generations</h1>
+                <p className="text-gray-400">Explore projects and contributions from your own creations.</p>
+            </header>
+
+            {/* Generations List */}
+            <div className="columns-1 sm:columns-2 lg:columns-3 gap-4">
+                {generations.map((project) => (
+                    <ProjectCard key={project.id} gen={project} setGenerations={setGenerations} forCommunity={false}/>
+                ))}
+            </div>
+
+            {generations.length === 0 && (
+                <div className="text-center py-20 bg-white/5 rounded-xl border border-white/10">
+                    <h3 className="text-xl font-medium mb-2">No generations yet</h3>
+                    <p className="text-gray-400 mb-2">start creating stunning product photos today</p>
+                    <PrimaryButton onClick={() => window.location.href='/generate'}>
+                        Create New Generation
+                    </PrimaryButton>
+                </div>
+            )}
+        </div>
     </div>
   )
 }
